f40acedea8c406ac1b099a4c186ceb97ed7772dd
unknown <mututadaniel54@gmail.com>
2024-11-15 01:59:10 -0800

    updating components

--- src/Components/AddtoCartproducts/Cartitem.jsx
+++ src/Components/AddtoCartproducts/Cartitem.jsx
@@ -1,7 +1,6 @@
 import React,{useContext} from 'react'
 import { ShopContext } from '../context/Shop-context'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faMinus } from '@fortawesome/free-solid-svg-icons'
 import { faAdd , faTrash , faMinus  } from '@fortawesome/free-solid-svg-icons'
 
 export const Cartitem = (props) => {
--- src/Components/AddtoCartproducts/Productsfile.js
+++ src/Components/AddtoCartproducts/Productsfile.js
@@ -1,18 +1,18 @@
 
 
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import Productsrent from "./Productsrent";
 import DataCards from "../Datafile/Datafile";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import axios from "axios";
+
 
 
 
 
 const  Productsfile = () => {
 
-const[data,setData] = useState([]);
+
   const [items,setItems] = useState(DataCards);
   const [searchQuery, setSearchQuery] = useState('');
 
